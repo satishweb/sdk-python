9c99cec88920c046149527316f49fa4bbebe51bc
Curtis Mason <cumason@bu.edu>
2020-08-07 17:33:06 -0400

    uploading test actions script
    Signed-off-by: Curtis Mason <cumason@bu.edu>

---
+++ pypi_packaging.py
@@ -0,0 +1,39 @@
+import pkg_resources
+import os
+
+# FORMAT: 1.x.x
+pypi_config = {
+    "version_target": "0.3.0",
+    "package_name": "cloudevents",
+}
+
+
+def createTag():
+    from git import Repo
+
+    # Get local pypi cloudevents version
+    published_pypi_version = pkg_resources.get_distribution(
+        pypi_config["package_name"]
+    ).version
+
+    # Ensure pypi and local package versions match
+    if pypi_config["version_target"] == published_pypi_version:
+        # Create local git tag
+        repo = Repo(os.getcwd())
+        repo.create_tag('github-action-test-tag')
+
+        # Push git tag to remote master
+        origin = repo.remote()
+        origin.push('github-action-test-tag')
+
+    else:
+        # PyPI publish likely failed
+        print(
+            f"Expected {pypi_config['package_name']}=={pypi_config['version_target']} "
+            f"but found {pypi_config['package_name']}=={published_pypi_version}"
+        )
+        exit(1)
+
+
+if __name__ == "__main__":
+    createTag()
